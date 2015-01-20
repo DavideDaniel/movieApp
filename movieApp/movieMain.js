b4984e0c30da30f8ac3ecc8f35bf26ffac65c632
DavideDaniel <davidedaniel@gmail.com>
2015-01-20 00:08:13 -0500

    redoing creation in dom

--- movieApp/movieMain.js
+++ movieApp/movieMain.js
@@ -4,111 +4,157 @@ var movies = [ "Boyhood", "Whiplash", "Nightcrawler", "Gloria", "Gravity",
 ]
 
 var counter = 0;
-
+var index = 0
 var firstImg = $( "#i1" )
     .first();
 var lastImg = $( "#i10" )
     .last();
 
+var allMain = document.querySelectorAll("div.main")
+
 var xhr = new XMLHttpRequest();
 
-var createPosters = function ( posterURL ) {
-    function frontHandler() {
-        $( '.back' )
-            .addClass( 'flip' );
-        displayInfo();
-    }
-    $( "div.front" )
-        .each( function () {
-            var li = $( "<li>", {
-                "class": "image",
-                "style": posterURL,
-            } );
-            $( this )
-                .append( li );
-            li.click( frontHandler );
-        } );
+var createFlipPanel = function () {
+    var main = document.querySelector("div.main");
+
+    var section = document.createElement("section")
+    
+    $(section).appendTo(main)
+
+
+
+    // var insidePanel = $('div', {
+    //     "class": "inside panel",
+    //     "id": "'m" + index + "'"
+    // });
+    // $(section).append(insidePanel);
 
 }
+createFlipPanel();
 
-var BackHandlers = function ( elem ) {}
-$( '.back' )
-    .click( function () {
-        $( '.image' )
-            .removeClass( 'flip' );
-    } )
+// var createPosters = function ( posterURL ) {
+//     $( "<li>", {
+//         "class": "image",
+//         "style": posterURL,
+//     } )
+//         .appendTo( 'div.content.front' );
+// $( document )
+//     .on( 'click', '.image', function () {
+//         alert( this.innerHTML );
+//     } );
+// }
 
-var movieGET = function () {
-    var url = "http://omdbapi.com/?t=" + movies[ counter ]
-    xhr.open( "GET", url );
+// // function frontHandler() {
+// //         $( '.back' )
+// //             .addClass( 'flip' );
+// //         displayInfo();
+// //     }
+// //     $( "div.front" )
+// //         .each( function () {
+// //             var li = $( "<li>", {
+// //                 "class": "image",
+// //                 "style": posterURL,
+// //             } );
+// //             $( this )
+// //                 .append( li );
+// //             li.click( frontHandler );
+// //         } );
 
-    xhr.addEventListener( 'load', function ( e ) {
-        var d = xhr.responseText
-        var parsed = JSON.parse( d );
-        var poster = 'background:url("' + parsed.Poster + '") no-repeat'
-        createPosters( poster )
-    } )
-    xhr.send();
-    counter++
-    while ( counter >= movies.length ) {
-        counter = 0;
-    }
-}
+// var BackHandlers = function ( elem ) {}
+// $( '.back' )
+//     .click( function () {
+//         $( '.image' )
+//             .removeClass( 'flip' );
+//     } )
 
-var generateMovieList = function () {
-    // var url = "http://omdbapi.com/?t="
-    movies.forEach( function ( name ) {
-    // xhr.open("GET", url+name)
-    movieGET();
-    loadMovie();
-    } )
-}
+// var movieGET = function () {
+//     var url = "http://omdbapi.com/?t=" + movies[ counter ]
+//     xhr.open( "GET", url );
 
-var loadMovie = function () {
-
-}
-
-var movieSend = function () {
-    xhr.send();
-}
-
-var searchMovie = function ( name ) {
-    var input = document.querySelector( "input" )
-    var safe_url = encodeURI( name )
-    var url = "http://omdbapi.com/?t=" + safe_url;
-}
-
-// var posterDisplay = function ( elem, parsed ) {
-//     xhr.addEventListener( 'load', function () {
+//     xhr.addEventListener( 'load', function ( e ) {
+//         var d = xhr.responseText
+//         var parsed = JSON.parse( d );
 //         var poster = 'background:url("' + parsed.Poster + '") no-repeat'
-//         console.log( poster );
-//         $( elem )
-//             .attr( 'style', poster )
+//         createPosters( poster )
+//         //     function frontHandler() {
+//         //     $( '.back' )
+//         //         .addClass( 'flip' );
+//         //     displayInfo();
+//         // }
+//         // $( "div.front" )
+//         //     .each( function () {
+//         //         var li = $( "<li>", {
+//         //             "class": "image",
+//         //             "style": poster,
+//         //         } );
+//         //         $( this )
+//         //             .append( li );
+//         //         li.click( frontHandler );
+//         //     } );
+//         // } )
+//         xhr.send();
+//         counter++
+//         while ( counter >= movies.length ) {
+//             counter = 0;
+//         }
+//     } )
+
+// }
+
+// var generateMovieList = function () {
+//     // var url = "http://omdbapi.com/?t="
+//     movies.forEach( function ( name ) {
+//         // xhr.open("GET", url+name)
+//         movieGET();
+
 //     } )
 // }
 
-// var loadNextMovie = function ( elem ) { // Potentially recursive danger!
-//     loadMovie( elem )
-//     if ( elem == lastImg ) {
-//         firstImg = lastImg.next();
-//     }
+// var loadMovie = function () {
 
 // }
 
-var displayInfo = function ( elem ) {
-    xhr.addEventListener( 'load', function ( e ) {
+// var movieSend = function () {
+//     xhr.send();
+// }
 
-        var d = xhr.responseText //comes in in JSON so you will have to parse it
-        var parsed = JSON.parse( d );
+// var searchMovie = function ( name ) {
+//     var input = document.querySelector( "input" )
+//     var safe_url = encodeURI( name )
+//     var url = "http://omdbapi.com/?t=" + safe_url;
+// }
 
-    } )
-}
+// // var posterDisplay = function ( elem, parsed ) {
+// //     xhr.addEventListener( 'load', function () {
+// //         var poster = 'background:url("' + parsed.Poster + '") no-repeat'
+// //         console.log( poster );
+// //         $( elem )
+// //             .attr( 'style', poster )
+// //     } )
+// // }
 
-$( document )
-    .ready( function () {
-        generateMovieList();
+// // var loadNextMovie = function ( elem ) { // Potentially recursive danger!
+// //     loadMovie( elem )
+// //     if ( elem == lastImg ) {
+// //         firstImg = lastImg.next();
+// //     }
 
-    } )
+// // }
+
+// var displayInfo = function ( elem ) {
+//     xhr.addEventListener( 'load', function ( e ) {
+
+//         var d = xhr.responseText //comes in in JSON so you will have to parse it
+//         var parsed = JSON.parse( d );
+
+//     } )
+// }
+
+// $( document )
+//     .ready( function () {
+//         generateMovieList();
+
+//     } )
 
 $( ".main" )
 
@@ -128,36 +174,34 @@ $( ".main" )
 
 } )
 
-$( 'li' )
-    .click( function () {
-        $( '.back' )
-            .addClass( 'flip' );
-        displayInfo()
-    } )
-$( '#i2' )
-    .click( function () {
-        $( '#m2' )
-            .addClass( 'flip' );
-        displayInfo()
-    } )
-$( '#i3' )
-    .click( function () {
-        $( '#m3' )
-            .addClass( 'flip' );
-        displayInfo()
-    } )
-$( '#b1' )
-    .click( function () {
-        $( '#m1' )
-            .removeClass( 'flip' );
-    } )
-$( '#b2' )
-    .click( function () {
-        $( '#m2' )
-            .removeClass( 'flip' );
-    } )
-$( '#b3' )
-    .click( function () {
-        $( '#m3' )
-            .removeClass( 'flip' );
-    } )
+
+
+// $( 'li' )
+//     .click( function () {
+//         $( '.back' )
+//             .addClass( 'flip' );
+//         displayInfo()
+//     } ) 
+//     $( '#i2' )
+//     .click( function () {
+//         $( '#m2' )
+//             .addClass( 'flip' );
+//         displayInfo()
+//     } ) $( '#i3' )
+//     .click( function () {
+//         $( '#m3' )
+//             .addClass( 'flip' );
+//         displayInfo()
+//     } ) $( '#b1' )
+//     .click( function () {
+//         $( '#m1' )
+//             .removeClass( 'flip' );
+//     } ) $( '#b2' )
+//     .click( function () {
+//         $( '#m2' )
+//             .removeClass( 'flip' );
+//     } ) $( '#b3' )
+//     .click( function () {
+//         $( '#m3' )
+//             .removeClass( 'flip' );
+//     } )
